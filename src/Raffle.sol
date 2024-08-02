804301ef4fc5b2c6fa2a6dadfe97084b88653965
MBrock_29 <mbrock19@gmail.com>
2024-08-02 09:49:13 +0100

    Chainlink VRF

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -1,24 +1,27 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.19;
 
+import {VRFConsumerBaseV2Plus} from "@chainlink/contracts/src/v0.8/vrf/dev/VRFConsumerBaseV2Plus.sol";
 
 contract Raffle {
     // Errors
     error Raffle__SendMoreToEnterRaffle();
-
-
     uint256 private immutable i_entranceFee;
     address payable[] private s_players;
+    uint256 private immutable i_interval;
+    uint256 private s_lastTimeStamp;
 
-    constructor(uint256 entranceFee) {
+    constructor(uint256 entranceFee, uint256 interval) {
         i_entranceFee = entranceFee;
+        i_interval = interval;
+        s_lastTimeStamp = block.timestamp;
     }
 
     // Events
     event RaffleEntered(address indexed player);
-    
+
     function enterRaffle() external payable {
-        if(msg.value < i_entranceFee) {
+        if (msg.value < i_entranceFee) {
             revert Raffle__SendMoreToEnterRaffle();
         }
         s_players.push(payable(msg.sender));
@@ -27,10 +30,26 @@ contract Raffle {
     }
 
     function pickWinner() external {
-        
+        if ((block.timestamp - s_lastTimeStamp) < i_interval) {
+            revert();
+        }
+        //   requestId = s_vrfCoordinator.requestRandomWords(
+        //     VRFV2PlusClient.RandomWordsRequest({
+        //         keyHash: s_keyHash,
+        //         subId: s_subscriptionId,
+        //         requestConfirmations: requestConfirmations,
+        //         callbackGasLimit: callbackGasLimit,
+        //         numWords: numWords,
+        //         extraArgs: VRFV2PlusClient._argsToBytes(
+        //             // Set nativePayment to true to pay for VRF requests with Sepolia ETH instead of LINK
+        //             VRFV2PlusClient.ExtraArgsV1({nativePayment: false})
+        //         )
+        //     })
+        // );
     }
 
-    function getEntranceFee() external view returns(uint256) {
+    // Getter functions
+    function getEntranceFee() external view returns (uint256) {
         return i_entranceFee;
     }
 }
