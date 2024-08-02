f0fa9bc3b458c7db6f8ad79c97a6e576273800ef
MBrock_29 <mbrock19@gmail.com>
2024-08-02 10:45:50 +0100

    Import issues

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.19;
+pragma solidity ^0.8.19;
 
 import {VRFConsumerBaseV2Plus} from "@chainlink/contracts/src/v0.8/vrf/dev/VRFConsumerBaseV2Plus.sol";
 
-contract Raffle {
+contract Raffle is VRFConsumerBaseV2Plus {
     // Errors
     error Raffle__SendMoreToEnterRaffle();
     uint256 private immutable i_entranceFee;
@@ -11,7 +11,11 @@ contract Raffle {
     uint256 private immutable i_interval;
     uint256 private s_lastTimeStamp;
 
-    constructor(uint256 entranceFee, uint256 interval) {
+    constructor(
+        uint256 entranceFee,
+        uint256 interval,
+        address vrfCoordinator
+    ) VRFConsumerBaseV2Plus(vrfCoordinator) {
         i_entranceFee = entranceFee;
         i_interval = interval;
         s_lastTimeStamp = block.timestamp;
@@ -33,19 +37,11 @@ contract Raffle {
         if ((block.timestamp - s_lastTimeStamp) < i_interval) {
             revert();
         }
-        //   requestId = s_vrfCoordinator.requestRandomWords(
-        //     VRFV2PlusClient.RandomWordsRequest({
-        //         keyHash: s_keyHash,
-        //         subId: s_subscriptionId,
-        //         requestConfirmations: requestConfirmations,
-        //         callbackGasLimit: callbackGasLimit,
-        //         numWords: numWords,
-        //         extraArgs: VRFV2PlusClient._argsToBytes(
-        //             // Set nativePayment to true to pay for VRF requests with Sepolia ETH instead of LINK
-        //             VRFV2PlusClient.ExtraArgsV1({nativePayment: false})
-        //         )
-        //     })
-        // );
+    }
+
+        // Implement fulfillRandomWords function
+    function fulfillRandomWords(uint256 requestId, uint256[] calldata randomWords) internal override {
+        // Your logic to handle the random words
     }
 
     // Getter functions
