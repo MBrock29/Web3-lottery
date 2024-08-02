e00d9db9cecd8dd43dd65d96f2a1e815fa15e893
MBrock_29 <mbrock19@gmail.com>
2024-08-02 08:45:55 +0100

    Adding events

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -3,22 +3,32 @@ pragma solidity 0.8.19;
 
 
 contract Raffle {
-
+    // Errors
     error Raffle__SendMoreToEnterRaffle();
 
+
     uint256 private immutable i_entranceFee;
+    address payable[] private s_players;
 
     constructor(uint256 entranceFee) {
         i_entranceFee = entranceFee;
     }
+
+    // Events
+    event RaffleEntered(address indexed player);
     
-    function enterRaffle() public payable {
+    function enterRaffle() external payable {
         if(msg.value < i_entranceFee) {
             revert Raffle__SendMoreToEnterRaffle();
         }
+        s_players.push(payable(msg.sender));
+
+        emit RaffleEntered(msg.sender);
     }
 
-    function pickWinner() public {}
+    function pickWinner() external {
+        
+    }
 
     function getEntranceFee() external view returns(uint256) {
         return i_entranceFee;
