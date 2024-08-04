5238e2599cd473eda7a441fa042700f3c832a09d
MBrock_29 <mbrock19@gmail.com>
2024-08-04 11:18:27 +0100

    Fixing failing test

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -27,10 +27,11 @@ contract HelperConfig is CodeConstants, Script {
     }
 
     NetworkConfig public localNetworkConfig;
-    mapping(uint256 chainId => NetworkConfig) public networkConfigs;
+    mapping(uint256 => NetworkConfig) public networkConfigs;
 
     constructor() {
         networkConfigs[ETH_SEPOLIA_CHAIN_ID] = getSepoliaEthConfig();
+        networkConfigs[LOCAL_CHAIN_ID] = getOrCreateAnvilEthConfig();
     }
 
     function getConfigByChainId(
@@ -38,8 +39,6 @@ contract HelperConfig is CodeConstants, Script {
     ) public returns (NetworkConfig memory) {
         if (networkConfigs[chainId].vrfCoordinator != address(0)) {
             return networkConfigs[chainId];
-        } else if (chainId == LOCAL_CHAIN_ID) {
-            // getOrCreateAnvilChain()
         } else {
             revert HelperConfig__InvalidChainId();
         }
@@ -68,7 +67,7 @@ contract HelperConfig is CodeConstants, Script {
         }
 
         vm.startBroadcast();
-        VRFCoordinatorV2_5Mock vrfCoorfinatorMock = new VRFCoordinatorV2_5Mock(
+        VRFCoordinatorV2_5Mock vrfCoordinatorMock = new VRFCoordinatorV2_5Mock(
             MOCK_BASE_FEE,
             MOCK_GAS_PRICE_LINK,
             MOCK_WEI_PER_UINT_LINK
@@ -78,10 +77,12 @@ contract HelperConfig is CodeConstants, Script {
         localNetworkConfig = NetworkConfig({
             entranceFee: 0.01 ether,
             interval: 30,
-            vrfCoordinator: address(vrfCoorfinatorMock),
+            vrfCoordinator: address(vrfCoordinatorMock),
             gasLane: 0x787d74caea10b2b357790d5b5247c2f63d1d91572a9846f780606e4d953677ae,
             callbackGasLimit: 500000,
             subscriptionId: 0
         });
+
+        return localNetworkConfig;
     }
 }
