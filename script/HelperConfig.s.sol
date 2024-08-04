0e3d8537a01cf1753278f34b62df3b481bc30c69
MBrock_29 <mbrock19@gmail.com>
2024-08-04 10:26:35 +0100

    Helper config script

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -4,6 +4,10 @@ pragma solidity 0.8.19;
 
 import {Script} from "forge-std/Script.sol";
 
+abstract contract CodeConstants {
+    uint256 public constant ETH_SEPOLIA_CHAIN_ID = 1115511;
+}
+
 contract HelperConfig is Script {
     struct NetworkConfig {
         uint256 entranceFee;
@@ -12,5 +16,24 @@ contract HelperConfig is Script {
         bytes32 gasLane;
         uint256 subscriptionId;
         uint32 callbackGasLimit;
+    }
+
+    NetworkConfig public localNetworkConfig;
+    mapping(uint256 chainId => NetworkConfig) public networkConfigs;
+
+    constructor() {
+        networkConfigs[ETH_SEPOLIA_CHAIN_ID] = getSepoliaEthConfig();
+    }
+
+    function getSepoliaEthConfig() public pure returns (NetworkConfig memory) {
+        return
+            NetworkConfig({
+                entranceFee: 0.02 ether,
+                interval: 30,
+                vrfCoordinator: 0xD7f86b4b8Cae7D942340FF628F82735b7a20893a,
+                gasLane: 0x8077df514608a09f83e4e8d300645594e5d7234665448ba83f51a50f842bd3d9,
+                callbackGasLimit: 500000,
+                subscriptionId: 0
+            });
     }
 }
