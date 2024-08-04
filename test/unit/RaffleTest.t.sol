c9e9c1d24c26f0a6080dde7e8d66a477e3d48da9
MBrock_29 <mbrock19@gmail.com>
2024-08-04 10:59:30 +0100

    Raffle Test

---
+++ test/unit/RaffleTest.t.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.19;
+
+import {Test} from "forge-std/Test.sol";
+import {DeployRaffle} from "script/DeployRaffle.s.sol";
+import {Raffle} from "src/Raffle.sol";
+import {HelperConfig} from "script/HelperConfig.s.sol";
+
+contract RaffleTest is Test {
+    Raffle public raffle;
+    HelperConfig public helperConfig;
+
+    address public PLAYER = makeAddr("player");
+
+    function setUp() external {
+        DeployRaffle deployer = new DeployRaffle();
+        (raffle, helperConfig) = deployer.deployContract();
+    }
+}
